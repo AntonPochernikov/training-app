3679d0b0f565575097fa807007d60fe4786c0cfa
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-04-05 13:20:49 +0300

    add card structure and added tasks to json

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -1,9 +1,44 @@
 import React, { Fragment } from 'react';
+import { Row, Card } from 'react-bootstrap';
 
 const HomePage = () => (
-    <Fragment>
-        Hello!
-    </Fragment>
+  <Fragment>
+    <Row>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Карточка 1 </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Text>
+                        описание
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Карточка 2</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Text>
+                        описание
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Карточка 3</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Text>
+                        описание
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    </Row>
+  </Fragment>
 );
 
 export default HomePage;
