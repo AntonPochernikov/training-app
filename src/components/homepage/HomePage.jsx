221c8dc5ef268e2bb0d6746a7e011ed3c6932cd6
Olga Bychkova <olga141293@yandex.ru>
2019-04-05 14:09:36 +0300

    corrected code

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -3,16 +3,13 @@ import { Row, Card } from 'react-bootstrap';
 
 const HomePage = () => (
   <Fragment>
-<<<<<<< HEAD
-        Hello!
-=======
     <Row>
       <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>Карточка 1 </Card.Title>
           <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
           <Card.Text>
-                        описание
+            описание
           </Card.Text>
           <Card.Link href="#">Card Link</Card.Link>
           <Card.Link href="#">Another Link</Card.Link>
@@ -23,7 +20,7 @@ const HomePage = () => (
           <Card.Title>Карточка 2</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
           <Card.Text>
-                        описание
+            описание
           </Card.Text>
           <Card.Link href="#">Card Link</Card.Link>
           <Card.Link href="#">Another Link</Card.Link>
@@ -34,14 +31,13 @@ const HomePage = () => (
           <Card.Title>Карточка 3</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
           <Card.Text>
-                        описание
+            описание
           </Card.Text>
           <Card.Link href="#">Card Link</Card.Link>
           <Card.Link href="#">Another Link</Card.Link>
         </Card.Body>
       </Card>
     </Row>
->>>>>>> feature/card
   </Fragment>
 );
 
