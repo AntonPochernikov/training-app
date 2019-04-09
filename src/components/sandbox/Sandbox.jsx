0bf4b5ef3b1e95214e7a468c03a3c0e1f65b7173
Olga Bychkova <olga141293@yandex.ru>
2019-04-09 10:55:52 +0300

    add grid layout to component sandbox and styles

--- src/components/sandbox/Sandbox.jsx
+++ src/components/sandbox/Sandbox.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import './Sandbox.css';
+import {
+  Button,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
 
 export default class Sandbox extends React.Component {
   handleCodeChange = (newValue) => {
@@ -10,14 +16,30 @@ export default class Sandbox extends React.Component {
   render() {
     const { code } = this.props;
     return (
-      <div className='sandbox'>
-        <MonacoEditor
-          language="javascript"
-          theme="vs-dark"
-          onChange={this.handleCodeChange}
-          value={code}
-        />
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <div className='sandbox'>
+              <MonacoEditor
+                language="javascript"
+                theme="vs-dark"
+                onChange={this.handleCodeChange}
+                value={code}
+                height='400px'
+              />
+              <Button variant="primary" className="btn-sandbox" size="sm">Отправить решение</Button>
+            </div>
+          </Col>
+          <Col>
+            <Row>
+              <Col className="sandbox-col1">текст задачи</Col>
+            </Row>
+            <Row>
+              <Col className="sandbox-col2">тест</Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
