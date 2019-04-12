1632ce1b294c5c7f6ad9e915679c0db381345769
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-04-12 18:11:54 +0300

    Setting page navigation
    -- added new page, set up page navigation, redirection when updating the sandbox to the 'HomePage'(in future 'Exercises') page
    -- TODO new 'HomePage' and rename old 'HomePage' in 'Exercises'

--- ui/src/components/MainRouter.jsx
+++ ui/src/components/MainRouter.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import HomePage from './homepage/HomePage.js';
 import Sandbox from './sandbox/Sandbox.js';
+import Tests from './tests/Tests.jsx';
+import Lessons from './lessons/Lessons.jsx';
 import MainHeader from './MainHeader.jsx';
 
 export default class MainRouter extends React.Component {
@@ -11,6 +13,14 @@ export default class MainRouter extends React.Component {
 
   renderRoot = () => <Redirect to='/home'/>
 
+  renderSandbox = () => {
+    const { currentTask } = this.props;
+    if (currentTask === null) {
+      return <Redirect to="/exercises"/>;
+    }
+    return <Sandbox/>;
+  }
+
   render() {
     return (
       <Router>
@@ -18,8 +28,10 @@ export default class MainRouter extends React.Component {
           <MainHeader />
           <main className='main'>
             <Route exact path='/' render={this.renderRoot}/>
-            <Route path='/home' component={HomePage} />
-            <Route path='/sandbox' component={Sandbox} />
+            <Route path='/exercises' component={HomePage} />
+            <Route path='/sandbox' render={this.renderSandbox}/>
+            <Route path='/tests' component={Tests} />
+            <Route path='/lessons' component={Lessons} />
           </main>
         </div>
       </Router>
