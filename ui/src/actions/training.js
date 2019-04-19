1a3a9b5f30ac7ac4f654a642514afaeec2327896
Anton Pochernikov <anton.pochernikov@accenture.com>
2019-04-19 14:36:55 +0300

    add mocha output styles

--- ui/src/actions/training.js
+++ ui/src/actions/training.js
@@ -1,6 +1,5 @@
 import { createAction } from 'redux-actions';
 import 'mocha/mocha';
-
 // eslint-disable-next-line
 import { assert } from 'chai';
 import * as selector from '../selectors';
@@ -10,17 +9,11 @@ window.assert = assert;
 mocha.setup('bdd');
 
 export const changeCode = createAction('CODE/CHANGE');
-export const getCurrentTask = createAction('TASK/ID/GET');
+export const getCurrentTask = createAction('TASK/CURRENT/GET');
 
 export const fetchDataRequest = createAction('DATA/FETCH/REQUEST');
 export const fetchDataSuccess = createAction('DATA/FETCH/SUCCESS');
 export const fetchDataFailure = createAction('DATA/FETCH/FAILURE');
-export const showModal = createAction('MODAL/SHOW');
-export const hideModal = createAction('MODAL/HIDE');
-export const changeEmail = createAction('EMAIL/CHANGE');
-export const changePassword = createAction('PASSWORD/CHANGE');
-export const loginSuccess = createAction('USER/LOGIN/SUCCESS');
-
 export const fetchData = () => async (dispatch) => {
   const source = null;
   dispatch(fetchDataRequest({ source }));
@@ -32,6 +25,13 @@ export const fetchData = () => async (dispatch) => {
     dispatch(fetchDataFailure({ error: e }));
   }
 };
+
+export const showModal = createAction('MODAL/SHOW');
+export const hideModal = createAction('MODAL/HIDE');
+
+export const changeEmail = createAction('EMAIL/CHANGE');
+export const changePassword = createAction('PASSWORD/CHANGE');
+export const loginSuccess = createAction('USER/LOGIN/SUCCESS');
 
 export const testSolution = () => async (dispatch, getState) => {
   // eslint-disable-next-line
