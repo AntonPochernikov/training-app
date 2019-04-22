2f112d5b55e52147d911c0df30f92607f2757663
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-04-22 13:10:52 +0300

    add action and reducer for test solution
    TODO: add a field fixing the current testing status.

--- ui/src/actions/training.js
+++ ui/src/actions/training.js
@@ -1,7 +1,6 @@
 import { createAction } from 'redux-actions';
 import 'mocha/mocha';
 
-// eslint-disable-next-line
 import { assert } from 'chai';
 import * as selector from '../selectors';
 
@@ -20,6 +19,9 @@ export const hideModal = createAction('MODAL/HIDE');
 export const changeEmail = createAction('EMAIL/CHANGE');
 export const changePassword = createAction('PASSWORD/CHANGE');
 export const loginSuccess = createAction('USER/LOGIN/SUCCESS');
+export const fetchTestRequest = createAction('TEST/FETCH/REQUEST');
+export const fetchTestSuccess = createAction('TEST/FETCH/SUCCESS');
+export const fetchTestFailure = createAction('TEST/FETCH/FAILURE');
 
 export const fetchData = () => async (dispatch) => {
   const source = null;
@@ -34,9 +36,18 @@ export const fetchData = () => async (dispatch) => {
 };
 
 export const testSolution = () => async (dispatch, getState) => {
+  dispatch(fetchTestRequest());
   // eslint-disable-next-line
   const { code } = getState().training;
   const { test } = selector.getTask(getState());
-  eval(test);
-  mocha.run();
+
+  try {
+    eval(test);
+    mocha
+      .run()
+      .on('pass', () => { dispatch(fetchTestSuccess()); })
+      .on('fail', () => { dispatch(fetchTestFailure()); });
+  } catch (e) {
+    console.log(e);
+  }
 };
