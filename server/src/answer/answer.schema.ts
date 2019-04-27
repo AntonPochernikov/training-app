45b0d464ef5d1134bab302d4971e4f2ba1c929ad
alexei.orlov <orel10181@gmail.com>
2019-04-27 17:38:51 +0300

    fix(question.schema): add unique flag in text column

--- server/src/answer/answer.schema.ts
+++ server/src/answer/answer.schema.ts
@@ -1,14 +1,14 @@
+import { QuestionToAnswer } from 'src/joins/QuestionToAnswer.schema'
+import { Field, ID, ObjectType } from 'type-graphql'
 import {
     BaseEntity,
     Column,
     CreateDateColumn,
     Entity,
+    OneToMany,
     PrimaryGeneratedColumn,
     UpdateDateColumn,
-    OneToMany,
 } from 'typeorm'
-import { Field, ID, ObjectType } from 'type-graphql'
-import { QuestionToAnswer } from 'src/joins/QuestionToAnswer.schema'
 
 @ObjectType()
 @Entity()
@@ -19,7 +19,7 @@ export class Answer extends BaseEntity {
     readonly id: number
 
     @Field()
-    @Column()
+    @Column({ unique: true })
     text: string
 
     // only db
