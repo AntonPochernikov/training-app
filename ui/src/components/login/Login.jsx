e0d5e400339a7d350ca64871610439565b1b722a
Olga Bychkova <olga141293@yandex.ru>
2019-05-15 18:30:44 +0300

    transformed component
    -- transformed functional component Login into a class component

--- ui/src/components/login/Login.jsx
+++ ui/src/components/login/Login.jsx
@@ -2,71 +2,70 @@ import React, { Fragment } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import './Login.css';
 
-// ждём email, password
-// лучше сделать класс
-const Login = ({ email, password, ...actions }) => {
-  const handleOverlayClick = (e) => {
-    e.preventDefault();
-    actions.hideModal();
-  };
+export default class Login extends React.Component {
+   handleOverlayClick = (e) => {
+     e.preventDefault();
+     this.props.hideModal();
+   };
 
-  const handleCloseButtonClick = (e) => {
-    e.preventDefault();
-    actions.hideModal();
-  };
+   handleCloseButtonClick = (e) => {
+     e.preventDefault();
+     this.props.hideModal();
+   };
 
-  const handleEmailChange = (e) => {
-    actions.changeEmail({ email: e.target.value });
-  };
+   handleEmailChange = (e) => {
+     this.props.changeEmail({ email: e.target.value });
+   };
 
-  const handlePasswordChange = (e) => {
-    actions.changePassword({ password: e.target.value });
-  };
+   handlePasswordChange = (e) => {
+     this.props.changePassword({ password: e.target.value });
+   };
 
-  const handleAddUser = () => {
-    // сделать request, реализовать success в thunk`e логина
-    actions.loginSuccess({ name: true });
-  };
+   handleAddUser = () => {
+     // сделать request, реализовать success в thunk`e логина
+     this.props.loginSuccess({ name: true });
+   };
 
-  return (
-    <Fragment>
-      <Modal.Dialog className="login-modal-wrapper">
-        <Modal.Header>
-          <Modal.Title>Вход</Modal.Title>
-          <button type= "reset" className="modal-button__close" onClick={handleCloseButtonClick}>
+   render() {
+     const { email, password } = this.props;
+     return (
+       <Fragment>
+         <Modal.Dialog className="login-modal-wrapper">
+           <Modal.Header>
+             <Modal.Title>Вход</Modal.Title>
+             <button type= "reset" className="modal-button__close" onClick={this.handleCloseButtonClick}>
             ×
-          </button>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Эл. почта</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Введите email"
-                onChange={handleEmailChange}
-                value={email}
-              />
-            </Form.Group>
+             </button>
+           </Modal.Header>
+           <Modal.Body>
+             <Form>
+               <Form.Group controlId="formBasicEmail">
+                 <Form.Label>Эл. почта</Form.Label>
+                 <Form.Control
+                   type="email"
+                   placeholder="Введите email"
+                   onChange={this.handleEmailChange}
+                   value={email}
+                 />
+               </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Пароль</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Введите пароль"
-                onChange={handlePasswordChange}
-                value={password}
-              />
-            </Form.Group>
-            <Button variant="primary" type="button" onClick={handleAddUser}>
+               <Form.Group controlId="formBasicPassword">
+                 <Form.Label>Пароль</Form.Label>
+                 <Form.Control
+                   type="password"
+                   placeholder="Введите пароль"
+                   onChange={this.handlePasswordChange}
+                   value={password}
+                 />
+               </Form.Group>
+               <Button variant="primary" type="button" onClick={this.handleAddUser}>
               Войти
-            </Button>
-          </Form>
-        </Modal.Body>
-      </Modal.Dialog>
-      <div className="login-modal-overlay" onClick={handleOverlayClick} />
-    </Fragment>
-  );
-};
-
-export default Login;
+               </Button>
+             </Form>
+           </Modal.Body>
+         </Modal.Dialog>
+         <div className="login-modal-overlay" onClick={this.handleOverlayClick} />
+       </Fragment>
+     );
+   }
+}
