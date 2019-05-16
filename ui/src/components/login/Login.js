62c41b44d1f0e794edaf9380b687d1175bebcd72
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-05-16 13:15:21 +0300

    add mapStateToProps for 'Login'

--- ui/src/components/login/Login.js
+++ ui/src/components/login/Login.js
@@ -3,7 +3,10 @@ import * as actionCreators from '../../actions';
 import Login from './Login.jsx';
 
 // передать email, password
-const mapStateToProps = () => ({});
+const mapStateToProps = state => ({
+  email: state.user.login.formFields.email,
+  password: state.user.login.formFields.password,
+});
 
 
 export default connect(mapStateToProps, actionCreators)(Login);
