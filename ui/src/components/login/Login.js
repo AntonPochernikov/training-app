22005c791a054d1c012d66e0b774db00ded7e9bb
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-05-16 13:18:46 +0300

    add clear monako editor  removed unused action (func clearCode)

--- ui/src/components/login/Login.js
+++ ui/src/components/login/Login.js
@@ -2,7 +2,6 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../../actions';
 import Login from './Login.jsx';
 
-// передать email, password
 const mapStateToProps = state => ({
   email: state.user.login.formFields.email,
   password: state.user.login.formFields.password,
