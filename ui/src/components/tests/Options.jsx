5e859ea5eb3a02802804a62f844b83ea45d23e00
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-05-29 17:36:34 +0300

    add new slyles for test components

--- ui/src/components/tests/Options.jsx
+++ ui/src/components/tests/Options.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Field } from 'redux-form';
+import './Options.css';
+
 
 const ChooseOne = (props) => {
   const {
     options,
-    isChecked,
   } = props;
+
   return options.map(({ id, description }) => (
     <div className="options" key={id}>
-      <label>
-        <Field
-          checked={isChecked}
-          name="options__input"
-          component="input"
-          type="radio"
-          value={id} />
-        {' '}
-        {description}
-      </label>
+      <Field
+        className="options-input"
+        checked="form.questions.fields.values.options_input" /* временно */
+        name="options__input"
+        component="input"
+        type="radio"
+        value={id}
+      />
+      <label className="options-label" forhtml="options__input"></label>
+      <span className="options-description">{description}</span>
     </div>
   ));
 };
