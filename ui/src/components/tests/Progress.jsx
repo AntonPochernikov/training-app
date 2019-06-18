2551de1d8f4f0f33541bbab1fa0741e1450692f3
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-06-18 12:59:56 +0300

    corrected code

--- ui/src/components/tests/Progress.jsx
+++ ui/src/components/tests/Progress.jsx
@@ -6,8 +6,10 @@ const Progress = (props) => {
     id,
     getQuestions,
   } = props;
-  return <div className="progress_bar">
-    <ProgressBar animated now={id} max={getQuestions.length}/>
-  </div>;
+  return (
+    <div className="progress_bar">
+      <ProgressBar animated now={id} max={getQuestions.length}/>
+    </div>
+  );
 };
 export default (Progress);
