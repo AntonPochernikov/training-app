0edea89c6871bba54660f87e0c038f3c7eba492f
Olga Bychkova <olga141293@yandex.ru>
2019-06-19 15:57:43 +0300

    add render of paragraphs content and styles

--- ui/src/components/lessons/Paragraphs.jsx
+++ ui/src/components/lessons/Paragraphs.jsx
@@ -5,12 +5,18 @@ import Navigation from './Navigation.js';
 export default class Paragraphs extends React.Component {
   render() {
     const {
-      paragraph: { id, number, name },
+      paragraph: {
+        id,
+        number,
+        name,
+        text,
+      },
     } = this.props;
     return (
       <div className="paragraphs-content">
         <div className="heading">
           <h2 className="heading-main">{number} {name}</h2>
+          <div className= "paragraph-text">{text}</div>
         </div>
         <Navigation
           id={id}
