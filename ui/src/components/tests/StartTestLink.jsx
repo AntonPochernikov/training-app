d6abb730e5996cbba2e4e08367ae7d5a57d477f6
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-06-20 17:00:10 +0300

    add 'zeroing' test questions when you start over or choose another test complexity

--- ui/src/components/tests/StartTestLink.jsx
+++ ui/src/components/tests/StartTestLink.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default class StartTestLink extends Component {
   handleClick = id => () => {
     this.props.getCurrentTestId({ testId: id });
+    this.props.getCurrentQuestionId({ questionId: 1 });
   }
 
   render() {
