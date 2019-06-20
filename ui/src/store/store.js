32c2fa2718471aa531dfd9852b90d7068936f8a8
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-06-20 15:21:51 +0300

    add application data storage in browser localstorage

--- ui/src/store/store.js
+++ ui/src/store/store.js
@@ -12,9 +12,28 @@ const reducers = combineReducers({ app, form });
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const loadState = () => {
+  try {
+    const serialisedState = localStorage.getItem('app_state');
+
+    if (!serialisedState) return undefined;
+
+    return JSON.parse(serialisedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const oldState = loadState();
+
 const store = createStore(
   reducers,
+  oldState,
   composeEnhancers(applyMiddleware(thunk)),
 );
+
+store.subscribe(() => {
+  localStorage.setItem('app_state', JSON.stringify(store.getState()));
+});
 
 export default store;
